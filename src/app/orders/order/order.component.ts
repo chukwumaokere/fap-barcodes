865f6680774926b3246e3d728fdbb96447b23ff3
chukwumaokere <chukwumaokere@yahoo.com>
2020-08-23 17:54:18 -0500

    adding order view

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-order',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
-  constructor() { }
+  orderid: any;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { this.orderid = this.route.snapshot.paramMap.get('orderid'); }
 
   ngOnInit(): void {
+    
+
   }
+  
 
 }
