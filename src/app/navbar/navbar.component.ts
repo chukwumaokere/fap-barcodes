5f7e7386434f642f1e0dba173a3343c01ec18ddc
chukwumaokere <chukwumaokere@yahoo.com>
2020-08-23 23:54:20 -0500

    updating navbar online detection

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+declare var $: any;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  online: Boolean;
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit(): void {
+    //offline detector
+    const setMsg = (flag) => {
+      //const p = document.getElementById('msg')
+      //p.innerHTML = '<b>Online?</b> ' + flag
+        var badge = document.getElementById('status');
+        if (flag === true){
+           badge.innerHTML="Online"; 
+            badge.classList.remove("badge-secondary");
+            badge.classList.remove("badge-danger");
+            badge.classList.add("badge-success");
+        }else{
+           badge.innerHTML="Offline"; 
+            badge.classList.remove("badge-secondary");
+            badge.classList.remove("badge-success");
+            badge.classList.add("badge-danger");
+        }
+    }
+
+    setMsg(navigator.onLine)
+
+    window.addEventListener("online", () => {
+      setMsg(true);
+    })
+    window.addEventListener("offline", () => {
+      setMsg(false);
+    })
   }
 
   logout(){
