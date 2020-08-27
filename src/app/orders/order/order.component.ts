f45761be81b48c90363d88c3871350e367306b78
tuyen.vv <tuyen.vv@boruapps.com>
2020-08-27 10:03:00 +0700

    #46391 - update detail page

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -8,40 +8,40 @@ import {OrderService} from '../../services/order.service';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-  orderid: any;
-  public orderData: any;
-  public orderItem: any;
+    orderid: any;
+    public orderData: any;
+    public orderItem: any;
+    public orderType: any;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    public orderService: OrderService
-  ) {
-    this.orderid = this.route.snapshot.paramMap.get('orderid');
-  }
-
-  ngOnInit(): void {
-    if (localStorage.getItem('userdata') !== '' && localStorage.getItem('userdata') !== null) {
-      this.loadOrderData();
-    } else {
-      this.logout();
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        public orderService: OrderService
+    ) {
+        this.orderid = this.route.snapshot.paramMap.get('orderid');
     }
 
-  }
-
-  logout(): void {
-    console.log('logging out');
-    try {
-      localStorage.removeItem('userdata');
-    } catch (err) {
-
+    ngOnInit(): void {
+        if (localStorage.getItem('userdata') !== '' && localStorage.getItem('userdata') !== null) {
+          this.loadOrderData();
+        } else {
+          this.logout();
+        }
     }
-    this.router.navigateByUrl('/login');
-  }
 
-  public async loadOrderData(): Promise<any> {
-    const order = await this.orderService.getOrderById(this.orderid);
-    this.orderData = order.order;
-    this.orderItem = order.items;
-  }
+    logout(): void {
+        console.log('logging out');
+        try {
+          localStorage.removeItem('userdata');
+        } catch (err) {
+        }
+        this.router.navigateByUrl('/login');
+    }
+
+    public async loadOrderData(): Promise<any> {
+        const order = await this.orderService.getOrderById(this.orderid);
+        this.orderData = order.order;
+        this.orderItem = order.items;
+        this.orderType = order.type;
+    }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -6,35 +6,40 @@ import {DatabaseService} from './database.service';
 })
 export class OrderService {
 
-  constructor(
-    public databaseService: DatabaseService
-  ) {
-  }
+    constructor(
+      public databaseService: DatabaseService
+    ) {
+    }
 
-  public async getOrderById(orderId): Promise<any> {
-      let order = {};
-      let item = {};
-      let orders;
-      let items;
-      let value;
-      const db = await this.databaseService.getDb();
-      await db.data.each((rows) => {
-          orders = rows.data;
-          items = rows.items;
-          for (const key in orders) {
-              if (orders.hasOwnProperty(key)) {
-                  value = orders[key];
-                  if (value[3] === orderId) {
-                    order = value;
-                    item = items[key];
-                    break;
-                  }
-              }
-          }
-      });
-      const data: {'order': any, 'items': any} = {order: [], items: []};
-      data.order = order;
-      data.items = item;
-      return data;
-  }
+    public async getOrderById(orderId): Promise<any> {
+        let order = {};
+        let item = {};
+        let type = '';
+        let orders;
+        let items;
+        let value;
+        let id;
+        const db = await this.databaseService.getDb();
+        await db.data.each((rows) => {
+            id = rows.id;
+            orders = rows.data;
+            items = rows.items;
+            for (const key in orders) {
+                if (orders.hasOwnProperty(key)) {
+                    value = orders[key];
+                    if (value[3] === orderId) {
+                        type = id;
+                        order = value;
+                        item = items[key];
+                        break;
+                    }
+                }
+            }
+        });
+        const data: {'type': any, 'order': any, 'items': any} = {type: '', order: [], items: []};
+        data.type = type;
+        data.order = order;
+        data.items = item;
+        return data;
+    }
 }
