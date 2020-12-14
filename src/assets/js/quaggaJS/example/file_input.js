073f18480d52b168c5ada9968713dd2714d8cbb5
tuyen.vv <tuyen.vv@boruapps.com>
2020-12-14 11:30:45 +0700

    #47343 - input wand popup

--- src/assets/js/quaggaJS/example/file_input.js
+++ src/assets/js/quaggaJS/example/file_input.js
@@ -246,6 +246,7 @@ $(function() {
     });
 
     $('#wand-input').on('input', function(){
+        var _this = $(this);
         var code = $(this).val();
         if(!code){
             return false;
@@ -291,6 +292,7 @@ $(function() {
                 $('#wand-input').val('');
             }
         }else{
+            _this.select();
             //$("#toast-body").html('Already Existing in array: ' + code);
             console.log('scannedFailedBarcodes on fail: ', scannedFailedBarcodes)
             var toast = $(".toast");
